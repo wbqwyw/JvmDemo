fd25a09ac2f65c74466ba64f873b997deb928742
wbq <wangbq3@yonyou.com>
2021-02-02 10:19:41 +0800

    jvm

---
+++ src/main/java/com/wbq/vm/IntegerTest.java
@@ -0,0 +1,28 @@
+package com.wbq.vm;
+
+/**
+ * 包名：com.wbq.vm
+ * 工程：JvmDemo
+ * 作者: wangbq
+ * 时间:  2021-02-02 上午10:00
+ */
+public class IntegerTest {
+
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
+
+    }
+
+}
