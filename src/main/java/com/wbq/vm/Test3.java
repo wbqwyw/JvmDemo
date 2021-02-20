274220131b5df174b9bb2cd5cbf4f9163a86eb00
wbq <wangbq3@yonyou.com>
2021-02-20 17:40:55 +0800

    jvm

---
+++ src/main/java/com/wbq/vm/Test3.java
@@ -0,0 +1,34 @@
+package com.wbq.vm;
+
+/**
+ * 包名：com.wbq.vm
+ * 工程：JvmDemo
+ * 作者: wangbq
+ * 时间:  2021-02-05 下午3:13
+ */
+public class Test3 {
+
+    public static void main(String[] args) {
+        //1
+//        int i = 10;
+//        i++;
+//        int j = 10;
+//        ++j;
+
+        //2
+//        int i1 = 10;
+//        int i2 = i1++;
+//        int i3 = 10;
+//        int i4 = ++i3;
+
+        //3
+        int i5 = 10;
+        i5 = i5++;
+        int i6 = 10;
+        i6 = ++i6;
+
+        //4
+
+
+    }
+}
